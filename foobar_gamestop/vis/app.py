43a05cf2021d06da3882282ab09de67a345e8478
Brian White <brijkw@gmail.com>
2021-03-28 14:47:24 -0700

    Implement layout of main dash app

--- foobar_gamestop/vis/app.py
+++ foobar_gamestop/vis/app.py
@@ -1,53 +1,28 @@
 import dash
-import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
-import pandas as pd
 
-from foobar_gamestop.avocados.data_utils import load_avocado_data
+from components import nav_bar, top_row, body_rows
 
-df = load_avocado_data()
-df = df.query("type == 'conventional' and region == 'Albany'")
-df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d")
-df.sort_values("Date", inplace=True)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app = dash.Dash(__name__)
 
 app.layout = html.Div(
-    children=[
-        html.H1(
-            children="Avocado Analytics",
-        ),
-        html.P(
-            children="Analyze the behavior of avocado prices"
-            " and the number of avocados sold in the US"
-            " between 2015 and 2018",
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": df["Date"],
-                        "y": df["AveragePrice"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Average Price of Avocados"},
-            },
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": df["Date"],
-                        "y": df["Total Volume"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Avocados Sold"},
-            },
+    [
+        nav_bar,
+        dbc.Container(
+            [
+                html.Br(),
+                top_row,
+                html.Br(),
+                body_rows,
+                html.Br(),
+                html.Div(style={"height": "200px"}),
+            ]
         ),
     ]
 )
 
+
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(port=8888, debug=True)
