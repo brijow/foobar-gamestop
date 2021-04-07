ad0644cd957673c2e429c807939b6bb60a61c7dd
Brian White <brijkw@gmail.com>
2021-04-07 14:14:47 -0700

    implement finnhub tag filter logic

--- foobar/scripts/comments_to_records.py
+++ foobar/scripts/comments_to_records.py
@@ -31,7 +31,11 @@ def raw_comments_csv_to_post_records():
         df = pp.clean_text_col(df, col="body")
         df = pp.perform_sentiment_analysis(df, col="body")
 
+        if df.empty:
+            continue
+
         tags_df = pp.perform_entity_extraction(df, col="body")
+        tags_df = pp.filter_tags_by_stock_tags(tags_df)
 
         df = pp.prep_comment_cols_for_db(df)
         df = pp.select_post_record_cols(df)
@@ -44,18 +48,26 @@ def raw_comments_csv_to_post_records():
 def save_tags_records(df, batch_num):
     if not df.empty:
         processed_data_dir = get_or_create_processed_data_dir()
-        filename = os.path.join(
-            processed_data_dir, "tags", "tagss_batch_" + str(batch_num)
-        )
+        data_dir = os.path.join(processed_data_dir, "tags")
+        dir_path = os.path.expanduser(data_dir)
+
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        filename = os.path.join(data_dir, "cmt_tags_" + str(batch_num) + ".csv")
         df.to_csv(filename)
 
 
 def save_comment_post_records(df, batch_num):
     if not df.empty:
         processed_data_dir = get_or_create_processed_data_dir()
-        filename = os.path.join(
-            processed_data_dir, "posts", "posts_batch_" + str(batch_num)
-        )
+        data_dir = os.path.join(processed_data_dir, "posts")
+        dir_path = os.path.expanduser(data_dir)
+
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        filename = os.path.join(data_dir, "cmt_posts_" + str(batch_num) + ".csv")
         df.to_csv(filename)
 
 
