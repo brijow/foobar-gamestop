653c849c1a06ee29ec375b95084bf5170c2d9ab1
Brian White <brijkw@gmail.com>
2021-04-09 14:10:28 -0700

    Finally able to build the entire post.csv and tag.csv

--- foobar/preprocessing/__init__.py
+++ foobar/preprocessing/__init__.py
@@ -4,6 +4,7 @@ Utilities to preprocess data
 
 from .reddit_preprocessing import (
     clean_text_col,
+    filter_bad_utcs,
     filter_by_cols,
     filter_by_date,
     filter_tags_by_stock_tags,
@@ -13,6 +14,7 @@ from .reddit_preprocessing import (
     prep_comment_cols_for_db,
     prep_submission_cols_for_db,
     select_post_record_cols,
+    utc_to_datetime,
 )
 
 __all__ = [
@@ -21,6 +23,8 @@ __all__ = [
     "perform_tag_extraction",
     "filter_by_cols",
     "filter_by_date",
+    "filter_bad_utcs",
+    "utc_to_datetime",
     "filter_tags_by_stock_tags",
     "clean_text_col",
     "prep_submission_cols_for_db",
