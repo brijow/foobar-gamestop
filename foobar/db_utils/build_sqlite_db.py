2446ee1e4e1dff57aac54a28375cff3552b329d3
Brian White <brijkw@gmail.com>
2021-04-09 15:38:40 -0700

    Populate post, gamestop, tag tables

--- foobar/db_utils/build_sqlite_db.py
+++ foobar/db_utils/build_sqlite_db.py
@@ -3,8 +3,12 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 
-from foobar.data_loader import get_root_data_dir, get_or_create_processed_data_dir
-from foobar.db_utils.models import Base, Tag
+from foobar.data_loader import (
+    get_or_create_processed_data_dir,
+    get_or_create_raw_data_dir,
+    get_root_data_dir,
+)
+from foobar.db_utils.models import Base, Gamestop, Post, Tag
 
 data_dir = get_root_data_dir()
 
@@ -32,7 +36,7 @@ def create_gamestop_db(force_refresh=False):
     Base.metadata.create_all(engine)
 
 
-def populate_posts_gamestop_db(limit_csvs=10):
+def populate_posts_gamestop_db():
 
     db_dir = _get_or_create_db_dir()
     file_path = os.path.join(db_dir, "gamestop.db")
@@ -41,36 +45,20 @@ def populate_posts_gamestop_db(limit_csvs=10):
     proc_dir = get_or_create_processed_data_dir()
     post_dir = os.path.join(proc_dir, "posts")
 
-    for cnt, f in enumerate(os.scandir(post_dir)):
-        if cnt > limit_csvs:
-            print(f"Inserted {cnt} csv files to db")
-            return
+    to_insert = pd.read_csv(post_dir + "/posts.csv")
 
-        if not (f.path.endswith(".csv") and f.is_file()):
-            continue
+    to_insert["dt"] = pd.to_datetime(to_insert["dt"])
 
-        to_insert = pd.read_csv(f.path)
-        to_insert = to_insert.drop("Unnamed: 0", axis=1)
+    to_insert = to_insert.drop_duplicates()
 
-        try:
-            to_insert.to_sql(name="post", con=engine, if_exists="append", index=False)
-            print("Successful insert!")
-        except Exception as e:
-            print("Initial failure to append: {}\n".format(e))
-            print("Attempting to rectify...")
-            existing = pd.read_sql("post", con=engine)
-            mask = ~to_insert.id.isin(existing.id)
-        try:
-            to_insert.loc[mask].to_sql(
-                name="post", con=engine, if_exists="append", index=False
-            )
-            print("Successful deduplication.")
-        except Exception as e2:
-            "Could not rectify duplicate entries. \n{}".format(e2)
-        print("Success after dedupe")
+    try:
+        engine.execute(Post.__table__.insert(), to_insert.to_dict("records"))
+        print("Successful insert!")
+    except Exception as e:
+        print("Initial failure to append: {}\n".format(e))
 
 
-def populate_tags_gamestop_db(limit_csvs=10):
+def populate_tags_gamestop_db():
 
     db_dir = _get_or_create_db_dir()
     file_path = os.path.join(db_dir, "gamestop.db")
@@ -79,22 +67,70 @@ def populate_tags_gamestop_db(limit_csvs=10):
     proc_dir = get_or_create_processed_data_dir()
     tag_dir = os.path.join(proc_dir, "tags")
 
-    for cnt, f in enumerate(os.scandir(tag_dir)):
-        if cnt > limit_csvs:
-            print(f"Inserted {cnt} csv files to db")
-            return
+    to_insert = pd.read_csv(tag_dir + "/tags.csv")
+    to_insert = to_insert.rename(columns={"id": "post_id"})
+    to_insert = to_insert.drop_duplicates()
 
-        if not (f.path.endswith(".csv") and f.is_file()):
-            continue
+    try:
+        engine.execute(Tag.__table__.insert(), to_insert.to_dict("records"))
+        print("Successful insert!")
+    except Exception as e:
+        print("Initial failure to append: {}\n".format(e))
 
-        to_insert = pd.read_csv(f.path)
-        to_insert = to_insert.drop("Unnamed: 0", axis=1)
-        to_insert = to_insert.rename(columns={"tag": "token"})
-        to_insert = to_insert.drop_duplicates()
+    # Helpful try-except left here just for an FYI
+    # try:
+    #     to_insert.to_sql(name="post", con=engine, if_exists="append", index=False)
+    #     print("Successful insert!")
+    # except Exception as e:
+    #     print("Initial failure to append: {}\n".format(e))
+    #     print("Attempting to rectify...")
+    #     existing = pd.read_sql("post", con=engine)
+    #     mask = ~to_insert.id.isin(existing.id)
+    # try:
+    #     to_insert.loc[mask].to_sql(
+    #         name="post", con=engine, if_exists="append", index=False
+    #     )
+    #     print("Successful deduplication.")
+    # except Exception as e2:
+    #     "Could not rectify duplicate entries. \n{}".format(e2)
+    # print("Success after dedupe")
 
-        try:
-            engine.execute(Tag.__table__.insert(), to_insert.to_dict("records"))
-            print("Successful insert!")
-        except Exception as e:
-            print("Initial failure to append: {}\n".format(e))
-            continue
+
+def populate_gamestop_gamestop_db():
+
+    db_dir = _get_or_create_db_dir()
+    file_path = os.path.join(db_dir, "gamestop.db")
+    engine = create_engine(f"sqlite:///{file_path}")
+
+    raw_dir = get_or_create_raw_data_dir()
+
+    to_insert = pd.read_csv(raw_dir + "/stock_candle_60_2020-03-01_2021-03-01.csv")
+    to_insert = to_insert.drop("Unnamed: 0", axis=1)
+    to_insert["hour"] = pd.to_datetime(to_insert["timestamp"], unit="s")
+    to_insert["prediction"] = -1.0
+    to_insert = to_insert.rename(
+        columns={
+            "close_price": "closeprice",
+            "high-price": "highprice",
+            "low-price": "lowprice",
+            "open_price": "openprice",
+        }
+    )
+    to_insert = to_insert[
+        [
+            "openprice",
+            "lowprice",
+            "highprice",
+            "closeprice",
+            "volume",
+            "hour",
+            "prediction",
+        ]
+    ]
+    to_insert = to_insert.drop_duplicates()
+
+    try:
+        engine.execute(Gamestop.__table__.insert(), to_insert.to_dict("records"))
+        print("Successful insert!")
+    except Exception as e:
+        print("Initial failure to append: {}\n".format(e))
