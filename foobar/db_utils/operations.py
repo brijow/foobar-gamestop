59335cde1335860a2c4e76ffe27202859ab7f312
Vignesh Perumal <vpa12@sfu.ca>
2021-04-10 16:35:28 -0700

    wide table created

--- foobar/db_utils/operations.py
+++ foobar/db_utils/operations.py
@@ -13,7 +13,6 @@ from treelib import Tree
 
 from foobar.db_utils.models import Base, Gamestop, Post, Tag
 
-
 def _get_or_create_db_dir():
     """Return the path of the db dir."""
     db_dir = os.environ.get(
@@ -27,6 +26,7 @@ def _get_or_create_db_dir():
 
 def connect_to_db():
     db_dir = _get_or_create_db_dir()
+    # db_dir = os.environ.get("FOOBAR_SQLITE_DIR")
     file_path = os.path.join(db_dir, "gamestop.db")
     if not os.path.isfile(file_path):
         print("Oops")
@@ -56,41 +56,20 @@ def get_posts_by_day(session, day1):
 def get_tags_for_postids(session, postids):
     return session.query(Tag).filter(Tag.post_id.in_(postids)).all()
 
-
-# def get_post_ids_for_tag(session, target_tags):
-#     return session.query(Tag).filter(Tag.token.in_(target_tags)).all()
-
-
 def join_post_tag_db_v2(session, day1):
     day2 = day1 + pd.DateOffset(1)
     return (
-        session.query(
-            Post.id,
-            Post.iscomment,
-            Post.submission_id,
-            Post.positive,
-            Post.negative,
-            Post.neutral,
-            Post.user,
-            Post.dt,
-            Tag.tag,
+        session.query(Post)
+        .join(Tag, 
+            Post.id == Tag.post_id,
+            isouter=True
         )
         .filter(
             Post.dt >= day1.to_pydatetime(),
-            Post.dt < day2.to_pydatetime(),
-            Post.id == Tag.post_id,
+            Post.dt < day2.to_pydatetime()            
         )
         .all()
     )
-
-
-# def join_post_tag_db(session):
-#     return session.query(
-#             Post.id, Post.iscomment, Post.submission_id,
-#             Post.positive, Post.negative, Post.neutral,
-#             Post.user, Post.dt, Tag.token
-#         ).filter(Post.id == Tag.post_id).all()
-
 
 def join_post_tag_df(df_tag, df_posts):
     df = df_posts.merge(df.join, how="left", left_on="id", right_on="post_id")
@@ -129,10 +108,6 @@ def get_aggregates_by_hour(df):
 def read_csvs(csvs_dir):
     big_df = pd.DataFrame()
     for cnt, f in enumerate(os.scandir(csvs_dir)):
-        # if cnt > limit_csvs:
-        #     print(f"Inserted {cnt} csv files to db")
-        #     return
-
         if not (f.path.endswith(".csv") and f.is_file()):
             continue
 
@@ -152,19 +127,22 @@ def fill_missing_hours(df, running_date):
 
 
 def make_reddit_hourly(running_date):
-    df = pd.DataFrame.from_records(join_post_tag_db_v2(s, i))
-    df.columns = [
-        "id",
-        "iscomment",
-        "submission_id",
-        "positive",
-        "negative",
-        "neutral",
-        "user",
-        "dt",
-        "tag",
-    ]
+    joined = join_post_tag_db_v2(s, i)
+    df = pd.DataFrame.from_records([i.__dict__ for i in joined],
+        columns=[
+                "id",
+                "iscomment",
+                "submission_id",
+                "positive",
+                "negative",
+                "neutral",
+                "user",
+                "dt",
+                "tag"],)
+    df['tag'] = df['tag'].fillna("<NOTAG>")
 
+    if df.empty:
+        return pd.DataFrame()
     df = round_to_hour(df, "dt")
     df_all = get_aggregates_by_hour(df)
 
@@ -192,20 +170,9 @@ def get_gamestop_hourly(session, day1):
 
 
 def build_wide_table(df_reddit, df_financial):
-    # df = pd.DataFrame.from_records(join_post_tag_db(session))
-    # df.columns = [
-    #     'id', 'iscomment', 'submission_id',
-    #     'positive', 'negative', 'neutral', 'user', 'dt', 'token'
-    # ]
-
-    # df = join_post_tag_df(df_tag, df_posts)
-
     df = df_reddit.join(df_financial, on="hour", lsuffix="_1", rsuffix="_2")
 
-    df = df.drop(columns=["hour_2"])
-
     df.columns = [
-        "hour",
         "avg_all_post_pos",
         "avg_all_post_neg",
         "avg_all_post_neu",
@@ -221,15 +188,13 @@ def build_wide_table(df_reddit, df_financial):
         "cnt_gme_post",
         "cnt_gme_comments",
         "id",
-        "openprice",
-        "lowprice",
-        "highprice",
         "volume",
+        "openprice",
         "closeprice",
+        "highprice",
+        "lowprice",    
         "prediction",
     ]
-    col = df.pop("id")
-    df.insert(0, col.name, col)
     return df
 
 
@@ -237,27 +202,23 @@ if __name__ == "__main__":
     s = connect_to_db()
     result = get_post_minmax_dates(s)
     start_date = result.start_date.replace(minute=0, hour=0, second=0, microsecond=0)
+    start_date = pd.to_datetime("2021-02-05 00:00:00")
+    print(start_date)
     end_date = result.end_date.replace(minute=0, hour=0, second=0, microsecond=0)
     post_cols = [c.name for c in Post.__table__.columns]
     tag_cols = [c.name for c in Tag.__table__.columns]
     gamestop_cols = [c.name for c in Gamestop.__table__.columns]
-    full_wide = pd.DataFrame()
-    # proc_dir = get_or_create_processed_data_dir()
-    # wide_csv = os.path.join(proc_dir, "wide.csv")
-    wide_csv = "wide.csv"
-    c = 0
+
+    proc_dir = get_or_create_processed_data_dir()
+    wide_csv = os.path.join(proc_dir, "wide.csv")
+    # wide_csv = "wide.csv"
+
     for i in pd.date_range(start_date, end_date, freq="D"):
-        if c < 8:
-            c += 1
-            continue
-        # tuplefied_posts = [(getattr(item, col) for col in post_cols) for item in get_posts_by_day(s, i)]
-        # df_daily_posts = pd.DataFrame.from_records(tuplefied_posts, columns=post_cols)
-
-        # tuplefied_tags = [(getattr(item, col) for col in tag_cols) for item in get_tags_for_postids(s, df_daily_posts['id'].tolist())]
-        # df_daily_tags = pd.DataFrame.from_records(tuplefied_tags, columns=tag_cols)
-        # build_wide_table(df_daily_posts, df_daily_tags, i)
-
         df_reddit = make_reddit_hourly(i)
+        if df_reddit.empty:
+            print("Reddit Holiday", i)
+            continue
+        df_reddit = df_reddit.set_index('hour')
 
         rows = get_gamestop_hourly(s, i)
         df_financial = pd.DataFrame.from_records(
@@ -265,19 +226,22 @@ if __name__ == "__main__":
             columns=[
                 "id",
                 "volume",
+                "openprice",
                 "closeprice",
                 "highprice",
-                "openprice",
+                "lowprice",
                 "prediction",
                 "hour",
             ],
         )
-
-        print(df_financial)
+        if df_financial.empty:
+            print("Market closed on ", str(i))
+            continue
 
         df_financial = (
             fill_missing_hours(df_financial, i).ffill().bfill()
         )  # Should I be doing this for missing data???
+        df_financial = df_financial.set_index('hour')
 
         df_wide = build_wide_table(df_reddit, df_financial)
 
@@ -285,12 +249,5 @@ if __name__ == "__main__":
             df_wide.to_csv(wide_csv)
         else:  # else it exists so append without writing the header
             df_wide.to_csv(wide_csv, mode="a", header=False)
-
-    # build_wide_table(s)
-    # proc_dir = get_or_create_processed_data_dir()
-    # post_dir = os.path.join(proc_dir, "posts")
-    # df_posts = read_csvs(post_dir)
-
-    # tag_dir = os.path.join(proc_dir, "tags")
-    # df_tag = read_csvs(tag_dir)
-    # wide_df = build_wide_table(df_tag, df_posts)
+        
+        
