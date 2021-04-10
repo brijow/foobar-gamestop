cf57dcb747c66969704f0dcc5013e8c5defa2bef
badkoubeh <b.badkoubeh@gmail.com>
2021-04-10 15:51:28 -0600

    query cassandra table, timestamp column name is added to the function

--- microservices/m1_pred_producer/predict_producer.py
+++ microservices/m1_pred_producer/predict_producer.py
@@ -15,9 +15,11 @@ import os
 GAMESTOP_TABLE = (
     os.environ.get("GAMESTOP_TABLE") if os.environ.get("GAMESTOP_TABLE") else "gamestop"
 )
-BUCKET = "bb-s3-bucket-cmpt733"
+TIMESTAMP_COLUMN = "timestamp_"
+
+BUCKET = os.environ.get("BUCKET_NAME") if os.environ.get("BUCKET_NAME") else "bb-s3-bucket-cmpt733"
 MODEL_FILE = "m1.pth"
-LOCAL_FILE = "microservices/m1_pred_producer/m2.pth"
+LOCAL_FILE = MODEL_FILE
 
 # Kafka producer
 KAFKA_BROKER_URL = (
@@ -37,55 +39,55 @@ producer = KafkaProducer(
 )
 # read historical data from cassandra and make predictions
 
+# last datetim with no prediction 
 last_poll_datetime = datetime.utcnow() - timedelta(hours=100)
 
-while True:
-    try:
-        train_result = download_model(BUCKET, MODEL_FILE, LOCAL_FILE)
-        # extract model parameters
-        feature_set = train_result["feature_set"]
-        history = train_result["history"]
-        prediction_horizon = int(train_result["pred_horizon"])
-        train_window = int(train_result["train_window"])
-        train_scaler = train_result["scaler"]
+try:
+    train_result = download_model(BUCKET, MODEL_FILE, LOCAL_FILE)
+    # extract model parameters
+    feature_set = train_result["feature_set"]
+    history = train_result["history"]
+    prediction_horizon = int(train_result["pred_horizon"])
+    train_window = int(train_result["train_window"])
+    train_scaler = train_result["scaler"]
 
-        num_features = len(feature_set) - 1
-        device = torch.device("cpu")
-        model = LSTM(input_size=num_features, seq_length=train_window)
-        model.load_state_dict(train_result["model"])
+    num_features = len(feature_set) - 1
+    device = torch.device("cpu")
+    model = LSTM(input_size=num_features, seq_length=train_window)
+    model.load_state_dict(train_result["model"])
 
-        query_from = last_poll_datetime
-        query_to = query_from + timedelta(hours=train_window)
-        df_gme = query_table(GAMESTOP_TABLE, query_from, query_to)
-        print(f"queried gme table from {query_from} to {query_to}")
+    query_from = last_poll_datetime
+    query_to = query_from + timedelta(hours=train_window)
+    df_gme = query_table(GAMESTOP_TABLE, TIMESTAMP_COLUMN, query_from, query_to)
+    print(f"queried gme table from {query_from} to {query_to}")
 
-        if df_gme is not None and not df_gme.empty:
-            # feature_set = ['openprice', 'highprice', 'lowprice', 'volume', 'closeprice']
-            gme_scaled = train_scaler.transform(df_gme[feature_set])
-            x = (
-                torch.tensor(gme_scaled[:train_window, :num_features])
-                .float()
-                .reshape(1, train_window, num_features)
-            )
-            # print(x.shape)
+    if df_gme is not None and not df_gme.empty:
+        # feature_set = ['openprice', 'highprice', 'lowprice', 'volume', 'closeprice']
+        gme_scaled = train_scaler.transform(df_gme[feature_set])
+        x = (
+            torch.tensor(gme_scaled[:train_window, :num_features])
+            .float()
+            .reshape(1, train_window, num_features)
+        )
+        # print(x.shape)
 
-            with torch.no_grad():
-                model.eval()
-                x.to(device)
-                model.init_hidden(1, device)
-                y_pred = model(x)
-                close_price_pred = y_pred.item()
-                print(f"Next hour close price predicted: {close_price_pred}")
-                prediction_dict = {
-                    "close_price_predict": close_price_pred,
-                    "timestamp_": query_to + timedelta(hours=prediction_horizon),
-                }
-                producer.send(TOPIC_NAME, value=prediction_dict)
+        with torch.no_grad():
+            model.eval()
+            x.to(device)
+            model.init_hidden(1, device)
+            y_pred = model(x)
+            close_price_pred = y_pred.item()
+            print(f"Next hour close price predicted: {close_price_pred}")
+            prediction_dict = {
+                "close_price_predict": close_price_pred,
+                "timestamp_": query_to + timedelta(hours=prediction_horizon),
+            }
+            producer.send(TOPIC_NAME, value=prediction_dict)
 
-        print("Next prediction...")
-        last_poll_datetime = query_to
+    print("Next prediction...")
+    last_poll_datetime = query_to
 
-    except Exception as e:
-        print(f"prediction failed. ERROR: {e}")
+except Exception as e:
+    print(f"prediction failed. ERROR: {e}")
 
-    time.sleep(SLEEP_TIME)
+
